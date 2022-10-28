227d6c29a523fd5ee3855cc25270b7fd17472863
Maidari Tsydenov <tmaidari@mail.ru>
2022-10-29 01:49:23 +0300

    asdasd

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # wifi_parse
+Этот код отправляет в телеграм сообщение с паролем от Wi-Fi, к которому Вы были подключены.
+
+# Установка
+1. Клонируйте репозиторий
+git clone https://github.com/MaidariTs/wifi_parse
+
+2. Установите вирутальное окружение
+python -m venv venv
+
+3. Активируйте виртуальное окружение
+source venv/Scripts/activate
+
+4. Установите requirements.txt
+pip install -r requirements.txt
+
+5. Создайте файл .env и впишите следующие переменные:
+BOT_TOKEN = 'Токен бота'
+USER_ID = 'ID пользователя'
+WIFI_NAME = 'Имя WIFI, к которому Вы были подключены'
---
+++ main.py
@@ -0,0 +1,50 @@
+from xml.dom import minidom
+from dotenv import load_dotenv
+import os
+import requests
+import subprocess
+
+
+load_dotenv()
+
+FILENAME_DIR = os.path.basename(__file__)
+PROJECT_DIR = os.path.abspath(__file__).replace(FILENAME_DIR, '')
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+USER_ID = os.getenv('USER_ID')
+WIFI_NAME = os.getenv('WIFI_NAME')
+
+
+subprocess.call("netsh wlan show profile")
+subprocess.call("netsh wlan export profile folder="
+                f"{PROJECT_DIR}\\data key=clear")
+
+
+def main():
+    for file in os.listdir("D:\\Dev\\wifi_parse\\data"):
+        if file.endswith(f"{WIFI_NAME}.xml"):
+            document = minidom.parse(f"D:\\Dev\\wifi_parse\\data\\{file}")
+
+            names = document.getElementsByTagName("name")
+            keyMaterials = document.getElementsByTagName("keyMaterial")
+
+            if names:
+                names = document.getElementsByTagName("name")[0]
+                name = names.firstChild.data
+
+            if keyMaterials:
+                keyMaterials = document.getElementsByTagName("keyMaterial")[0]
+                keyMaterial = keyMaterials.firstChild.data
+
+            wifi = {
+                "name": name,
+                "password": keyMaterial
+            }
+
+            print(wifi)
+            requests.post(f"https://api.telegram.org/{BOT_TOKEN}/"
+                          f"sendMessage?chat_id={USER_ID}&text={wifi}")
+
+
+if __name__ == '__main__':
+    main()
